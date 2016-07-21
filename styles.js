5a61fab05e847b3aa43c7b3be7119c5ebb95e489
MandyAmbroise <mandesa.ambroise@yahoo.com>
2016-07-21 09:56:06 -0700

    Web

---
+++ styles.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to display an alert box:</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+    alert("I am an alert box!");
+}
+</script>
+
+</body>
+</html>
+
+
+   
+   
+   
+   
+window.alert("Intrested in seeing more?";
